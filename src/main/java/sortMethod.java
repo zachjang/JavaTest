cbb192c7755ede37a3b395872ae2da0b34da5bb2
Zach Jang <zach.h.jang@gmail.com>
2018-10-25 20:17:57 +0800

    Add heapSort, countingSort and shellSort

--- src/main/java/sortMethod.java
+++ src/main/java/sortMethod.java
@@ -39,7 +39,7 @@ class quickSort
       low  --> Starting index,
       high  --> Ending index */
 
-    int[] sort(int array[], int low, int high) // Modified from void to int[].
+    int[] sort(int array[], int low, int high)
     {
         if (low < high)
         {
@@ -76,4 +76,138 @@ class insertionSort
         }
         return array;
     }
+}
+
+
+//Java program for implementation of Heap Sort
+class heapSort
+{
+    int[] sort(int array[])
+    {
+        int n = array.length;
+
+        // Build heap (rearrange array)
+        for (int i = n / 2 - 1; i >= 0; i--)
+            heapify(array, n, i); 
+
+        // One by one extract an element from heap
+        for (int i=n-1; i>=0; i--)
+        {
+            // Move current root to end
+            int temp = array[0];
+            array[0] = array[i];
+            array[i] = temp;
+
+            // call max heapify on the reduced heap
+            heapify(array, i, 0);
+        }
+        return array;
+    }
+
+     // To heapify a subtree rooted with node i which is
+     // an index in array[]. n is size of heap
+     void heapify(int array[], int n, int i)
+     {
+         int largest = i; // Initialize largest as root
+         int l = 2*i + 1; // left = 2*i + 1
+         int r = 2*i + 2; // right = 2*i + 2
+
+         // If left child is larger than root
+         if (l < n && array[l] > array[largest])
+             largest = l;
+
+         // If right child is larger than largest so far
+         if (r < n && array[r] > array[largest])
+             largest = r;
+
+         // If largest is not root
+         if (largest != i)
+         {
+             int swap = array[i];
+             array[i] = array[largest];
+             array[largest] = swap;
+
+             // Recursively heapify the affected sub-tree
+             heapify(array, n, largest);
+         }
+     }
+ }
+
+//Java implementation of Counting Sort
+class countingSort
+{
+	int[] sort(int array[])
+	{
+	    int n = array.length;
+
+	    // The output character array that will have sorted array
+		int output[] = new int[n];
+		
+		// Create a count array to store count of individual
+		// characters and initialize count array as 0
+		int count[] = new int[256];
+		for (int i=0; i<256; ++i)
+		    count[i] = 0;
+		
+		// store count of each character
+		for (int i=0; i<n; ++i)
+		    ++count[array[i]];
+		
+		// Change count[i] so that count[i] now contains actual
+		// position of this character in output array
+		for (int i=1; i<=255; ++i)
+		    count[i] += count[i-1];
+		
+		// Build the output character array
+		// To make it stable we are operating in reverse order.
+		for (int i = n-1; i>=0; i--)
+		{
+		    output[count[array[i]]-1] = array[i];
+		    --count[array[i]];
+		}
+		
+		// Copy the output array to array, so that array now
+		// contains sorted characters
+		    for (int i = 0; i<n; ++i)
+		        array[i] = output[i];
+		return array;
+	}
+}
+/*This code is contributed by Rajat Mishra */
+
+//Java implementation of ShellSort
+class shellSort
+{
+	/* function to sort array using shellSort */
+	int[] sort(int array[])
+	{
+		int n = array.length;
+
+		// Start with a big gap, then reduce the gap
+		for (int gap = n/2; gap > 0; gap /= 2)
+		{
+			// Do a gaped insertion sort for this gap size.
+			// The first gap elements a[0..gap-1] are already
+			// in gaped order keep adding one more element
+			// until the entire array is gap sorted
+			for (int i = gap; i < n; i += 1)
+			{
+				// add a[i] to the elements that have been gap
+				// sorted save a[i] in temporary array and make a hole at
+				// position i
+				int temp = array[i];
+
+				// shift earlier gap-sorted elements up until
+				// the correct location for a[i] is found
+				int j;
+				for (j = i; j >= gap && array[j - gap] > temp; j -= gap)
+					array[j] = array[j - gap];
+
+				// put temporary array (the original a[i]) in its correct
+				// location
+				array[j] = temp;
+			}
+		}
+		return array;
+	}
 }
--- src/test/java/testSort.java
+++ src/test/java/testSort.java
@@ -15,7 +15,7 @@ public class testSort {
     }
 
     @Test
-    public void testInserton() {
+    public void testInsertion() {
         System.out.println("selection:");
         insertionSort sort = new insertionSort();
         assertArrayEquals(new int[] {1,2,3}, sort.insertion(new int[] {3,1,2}));
@@ -27,19 +27,60 @@ public class testSort {
         assertArrayEquals(new int[] {1,2,3,4,5,6,7,8,9}, sort.insertion(new int[] {3,7,9,1,8,6,4,2,5}));
         assertArrayEquals(new int[] {1,2,3,4,5,6,7,8,9,10}, sort.insertion(new int[] {3,7,9,1,8,6,4,2,10,5}));
         }
-    
+
     @Test
     public void testQuick() {
-    	System.out.println("quick:");
-    	quickSort ob = new quickSort();
-    	assertArrayEquals(new int[] {1,2,3}, ob.sort(new int[] {3,1,2}, 0, 2));
-    	assertArrayEquals(new int[] {1,2,3,4}, ob.sort(new int[] {3,1,4,2}, 0, 3));
-    	assertArrayEquals(new int[] {1,2,3,4,5}, ob.sort(new int[] {3,5,1,4,2}, 0, 4));
-    	assertArrayEquals(new int[] {1,2,3,4,5,6}, ob.sort(new int[] {6,4,5,3,1,2}, 0, 5));
-    	assertArrayEquals(new int[] {1,2,3,4,5,6,7}, ob.sort(new int[] {4,5,7,6,3,1,2}, 0, 6));
-    	assertArrayEquals(new int[] {1,2,3,4,5,6,7,8}, ob.sort(new int[] {7,8,4,5,6,3,1,2}, 0, 7));
-    	assertArrayEquals(new int[] {1,2,3,4,5,6,7,8,9}, ob.sort(new int[] {6,4,9,5,7,8,3,1,2}, 0, 8));
-    	assertArrayEquals(new int[] {1,2,3,4,5,6,7,8,9,10}, ob.sort(new int[] {10,4,5,8,9,6,7,3,1,2}, 0, 9));
-    	}
+        System.out.println("quick:");
+        quickSort ob = new quickSort();
+        assertArrayEquals(new int[] {1,2,3}, ob.sort(new int[] {3,1,2}, 0, 2));
+        assertArrayEquals(new int[] {1,2,3,4}, ob.sort(new int[] {3,1,4,2}, 0, 3));
+        assertArrayEquals(new int[] {1,2,3,4,5}, ob.sort(new int[] {3,5,1,4,2}, 0, 4));
+        assertArrayEquals(new int[] {1,2,3,4,5,6}, ob.sort(new int[] {6,4,5,3,1,2}, 0, 5));
+        assertArrayEquals(new int[] {1,2,3,4,5,6,7}, ob.sort(new int[] {4,5,7,6,3,1,2}, 0, 6));
+        assertArrayEquals(new int[] {1,2,3,4,5,6,7,8}, ob.sort(new int[] {7,8,4,5,6,3,1,2}, 0, 7));
+        assertArrayEquals(new int[] {1,2,3,4,5,6,7,8,9}, ob.sort(new int[] {6,4,9,5,7,8,3,1,2}, 0, 8));
+        assertArrayEquals(new int[] {1,2,3,4,5,6,7,8,9,10}, ob.sort(new int[] {10,4,5,8,9,6,7,3,1,2}, 0, 9));
+        }
 
-    }
+    @Test
+    public void testHeap() {
+        System.out.println("heap");
+        heapSort ob = new heapSort();
+        assertArrayEquals(new int[] {1,2,3}, ob.sort(new int[] {3,1,2}));
+        assertArrayEquals(new int[] {1,2,3,4}, ob.sort(new int[] {3,1,4,2}));
+        assertArrayEquals(new int[] {1,2,3,4,5}, ob.sort(new int[] {3,5,1,4,2}));
+        assertArrayEquals(new int[] {1,2,3,4,5,6}, ob.sort(new int[] {6,4,5,3,1,2}));
+        assertArrayEquals(new int[] {1,2,3,4,5,6,7}, ob.sort(new int[] {4,5,7,6,3,1,2}));
+        assertArrayEquals(new int[] {1,2,3,4,5,6,7,8}, ob.sort(new int[] {7,8,4,5,6,3,1,2}));
+        assertArrayEquals(new int[] {1,2,3,4,5,6,7,8,9}, ob.sort(new int[] {6,4,9,5,7,8,3,1,2}));
+        assertArrayEquals(new int[] {1,2,3,4,5,6,7,8,9,10}, ob.sort(new int[] {10,4,5,8,9,6,7,3,1,2}));
+        }
+
+    @Test
+    public void testCounting() {
+        System.out.println("counting");
+        countingSort ob = new countingSort();
+        assertArrayEquals(new int[] {1,2,3}, ob.sort(new int[] {3,1,2}));
+        assertArrayEquals(new int[] {1,2,3,4}, ob.sort(new int[] {3,1,4,2}));
+        assertArrayEquals(new int[] {1,2,3,4,5}, ob.sort(new int[] {3,5,1,4,2}));
+        assertArrayEquals(new int[] {1,2,3,4,5,6}, ob.sort(new int[] {6,4,5,3,1,2}));
+        assertArrayEquals(new int[] {1,2,3,4,5,6,7}, ob.sort(new int[] {4,5,7,6,3,1,2}));
+        assertArrayEquals(new int[] {1,2,3,4,5,6,7,8}, ob.sort(new int[] {7,8,4,5,6,3,1,2}));
+        assertArrayEquals(new int[] {1,2,3,4,5,6,7,8,9}, ob.sort(new int[] {6,4,9,5,7,8,3,1,2}));
+        assertArrayEquals(new int[] {1,2,3,4,5,6,7,8,9,10}, ob.sort(new int[] {10,4,5,8,9,6,7,3,1,2}));
+        }
+    
+    @Test
+    public void testShell() {
+    	System.out.println("shell");
+    	shellSort ob = new shellSort();
+        assertArrayEquals(new int[] {1,2,3}, ob.sort(new int[] {3,1,2}));
+        assertArrayEquals(new int[] {1,2,3,4}, ob.sort(new int[] {3,1,4,2}));
+        assertArrayEquals(new int[] {1,2,3,4,5}, ob.sort(new int[] {3,5,1,4,2}));
+        assertArrayEquals(new int[] {1,2,3,4,5,6}, ob.sort(new int[] {6,4,5,3,1,2}));
+        assertArrayEquals(new int[] {1,2,3,4,5,6,7}, ob.sort(new int[] {4,5,7,6,3,1,2}));
+        assertArrayEquals(new int[] {1,2,3,4,5,6,7,8}, ob.sort(new int[] {7,8,4,5,6,3,1,2}));
+        assertArrayEquals(new int[] {1,2,3,4,5,6,7,8,9}, ob.sort(new int[] {6,4,9,5,7,8,3,1,2}));
+        assertArrayEquals(new int[] {1,2,3,4,5,6,7,8,9,10}, ob.sort(new int[] {10,4,5,8,9,6,7,3,1,2}));
+    	}
+}
